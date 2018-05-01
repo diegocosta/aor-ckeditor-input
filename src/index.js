8d84f2be90325ed8b87802dd0ccece58a5361e52
Diego Costa <diego@diegocosta.com.br>
2018-05-01 18:17:36 -0300

    Added error messages

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import get from 'lodash.get';
 import CKEditor from 'react-ckeditor-component';
 
 const CKEditorInput = props => (
-  <CKEditor
-    content={get(props.record, props.source)}
-    events={{
-      change: e => props.input.onChange(e.editor.getData()),
-    }}
-    {...props}
-  />
+  <div className="aor-ckeditor-input">
+    <CKEditor
+      content={get(props.record, props.source)}
+      events={{
+        change: e => props.input.onChange(e.editor.getData()),
+      }}
+      {...props}
+    />
+    {(props.meta.touched && props.meta.error) &&
+      <div className="error">{props.meta.error}</div>}
+  </div>
 );
 
 CKEditorInput.defaultProps = {
