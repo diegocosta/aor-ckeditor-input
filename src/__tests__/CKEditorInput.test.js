3ec3869d68d2dbb4a8cdf844523c3599072dd684
Diego Costa <diego@diegocosta.com.br>
2018-05-02 13:36:37 -0300

    Updated test suite

--- src/__tests__/CKEditorInput.test.js
+++ src/__tests__/CKEditorInput.test.js
@@ -4,15 +4,40 @@ import renderer from 'react-test-renderer';
 
 import CKEditorInput from '../index';
 
+const props = {
+  meta: {},
+};
+
 describe('CKEditorInput', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<CKEditorInput />, div);
+    ReactDOM.render(<CKEditorInput {...props} />, div);
   });
 
   it('renders activeClass correctly', () => {
+    const localProps = {
+      ...props,
+      activeClass: 'aor-test',
+    };
+
     const tree = renderer
-      .create(<CKEditorInput activeClass="aor-test" />)
+      .create(<CKEditorInput {...localProps} />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders error message correctly', () => {
+    const localProps = {
+      ...props,
+      meta: {
+        touched: true,
+        error: 'Required',
+      },
+    };
+
+    const tree = renderer
+      .create(<CKEditorInput {...localProps} />)
       .toJSON();
 
     expect(tree).toMatchSnapshot();
